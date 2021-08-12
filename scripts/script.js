afb6d245ca8e3734b17808d562c1774353f6fa2f
Sidney <sidney_carlini@hotmail.com>
2021-08-12 20:58:54 -0300

    Adicionado table com mais informacoes sobre os vips.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
 $(".card-loja").click(function(){
     $(this).toggleClass("flip");
 });
+
+//Changes the advantages colors at donation's page
+var tableBody = document.getElementById("table-body");
+for(tableChildren of tableBody.children){
+    for(element of tableChildren.children){
+        if(element.innerText == "Sim"){
+            element.classList.add("text-green")
+        }else if(element.innerText == "Não"){
+            element.classList.add("text-reddanger")
+        }
+    }
+}
